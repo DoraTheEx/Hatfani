0e0eb550a23e8964f327cee568ea220e10475720
DoraTheEx <161976368+DoraTheEx@users.noreply.github.com>
2024-03-17 15:05:13 +0200

    Update Shronk.c

--- Shronk.c
+++ Shronk.c
@@ -1,97 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <winsock2.h>
 
 #define MAX_NODES 100
 #define PORT 12345
 
-// Structure to hold information about each node
-typedef struct {
-    char ip[16]; // Assuming IPv4 addresses
-    char parent_ip[16]; // Parent node's IP
+typedef struct Node {
+    char ip[16]; // IP address
+    int parent_index; // Index of the parent node in the array
 } Node;
 
-Node nodes[MAX_NODES]; // Array to store nodes
-int num_nodes = 0; // Number of nodes
+Node nodes[MAX_NODES];
+int num_nodes = 0;
 
-// Function to print family tree with proper indentation
-void printFamilyTree(int index, int level) {
-    for (int i = 0; i < level; i++) {
-        printf("  "); // Two spaces for each level of indentation
+void addNode(char* ip, int parent_index) {
+    if (num_nodes < MAX_NODES) {
+        strcpy(nodes[num_nodes].ip, ip);
+        nodes[num_nodes].parent_index = parent_index;
+        num_nodes++;
+    } else {
+        printf("Maximum number of nodes reached.\n");
     }
-    printf("%s\n", nodes[index].ip);
+}
+
+void printFamilyTree() {
+    printf("Family Tree:\n");
     for (int i = 0; i < num_nodes; i++) {
-        if (strcmp(nodes[i].parent_ip, nodes[index].ip) == 0) {
-            printFamilyTree(i, level + 1); // Recursive call for child nodes
+        int indent = 0;
+        int current_index = i;
+        while (current_index != -1) {
+            for (int j = 0; j < indent; j++) {
+                printf("  "); // Two spaces for each level of indentation
+            }
+            printf("%s\n", nodes[current_index].ip);
+            current_index = nodes[current_index].parent_index;
+            indent++;
         }
+        printf("\n");
     }
 }
 
 int main() {
     WSADATA wsaData;
-    SOCKET sock;
-    struct sockaddr_in server_addr;
-    int addr_len = sizeof(struct sockaddr_in);
-
-    // Initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        fprintf(stderr, "WSAStartup failed\n");
+        printf("WSAStartup failed.\n");
         return 1;
     }
 
-    // Create socket
-    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET) {
-        fprintf(stderr, "Socket creation failed\n");
-        return 1;
-    }
+    SOCKET server_socket;
+    struct sockaddr_in server_addr;
 
-    // Bind to port
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(PORT);
-
-    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        fprintf(stderr, "Bind failed\n");
-        closesocket(sock);
+    server_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (server_socket == INVALID_SOCKET) {
+        printf("Error creating socket.\n");
         WSACleanup();
         return 1;
     }
 
-    printf("Listening for connections...\n");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(PORT);
 
-    // Receive and process data
-    while (1) {
-        char buffer[32];
-        struct sockaddr_in client_addr;
-
-        if (recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &addr_len) == SOCKET_ERROR) {
-            fprintf(stderr, "Error in recvfrom\n");
-            closesocket(sock);
-            WSACleanup();
-            return 1;
-        }
-
-        // Extract sender IP and parent IP
-        char sender_ip[16];
-        char parent_ip[16];
-        sscanf(buffer, "%s %s", sender_ip, parent_ip);
-
-        // Store the node information
-        strcpy(nodes[num_nodes].ip, sender_ip);
-        strcpy(nodes[num_nodes].parent_ip, parent_ip);
-        num_nodes++;
-
-        // Print family tree
-        printf("Family Tree:\n");
-        for (int i = 0; i < num_nodes; i++) {
-            if (strcmp(nodes[i].parent_ip, "") == 0) { // Root node
-                printFamilyTree(i, 0);
-            }
-        }
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        printf("Bind failed.\n");
+        closesocket(server_socket);
+        WSACleanup();
+        return 1;
     }
 
-    closesocket(sock);
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    char sender_ip[16];
+    char parent_ip[16];
+
+    while (1) {
+        recvfrom(server_socket, sender_ip, sizeof(sender_ip), 0, (struct sockaddr *)&client_addr, &client_addr_len);
+        recvfrom(server_socket, parent_ip, sizeof(parent_ip), 0, (struct sockaddr *)&client_addr, &client_addr_len);
+
+        // Assume you have some function to determine the parent index based on the parent IP
+        int parent_index = 0; // Placeholder for parent index
+        addNode(sender_ip, parent_index);
+        printFamilyTree();
+    }
+
+    closesocket(server_socket);
     WSACleanup();
     return 0;
 }
